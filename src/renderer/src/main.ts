d2020ce8a36a9bc4fd18a854310683b7c9c75373
r0manch1k <romansokolv209@gmail.com>
2024-11-28 10:06:10 +1000

    fix

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -3,7 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import { GameScene } from './GameScene/GameScene.jse.js'
+import { GameScene } from './GameScene/GameScene.js'
 
 function main() {
     // Get a reference to the container element
